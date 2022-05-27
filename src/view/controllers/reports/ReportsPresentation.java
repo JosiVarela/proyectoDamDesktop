12744a980dca962abb0e4005a80be9f4ae4d811b
José Manuel Varela Vecino <josi.varela18@gmail.com>
2022-05-28 00:24:51 +0200

    Internationalizated reports screen

--- src/view/controllers/reports/ReportsPresentation.java
+++ src/view/controllers/reports/ReportsPresentation.java
@@ -1,5 +1,6 @@
 package view.controllers.reports;
 
+import controller.Translatable;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -9,17 +10,32 @@ import javafx.scene.layout.TilePane;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class ReportsPresentation implements Initializable {
+public class ReportsPresentation implements Initializable, Translatable {
     private ResourceBundle rb;
 
     @FXML
+    private Label lblNumberReportName;
+
+    @FXML
     private BorderPane numberReportCol;
+
+    @FXML
+    private Label lblNumberReportCol;
 
     @FXML
     private BorderPane colReportName;
 
     @FXML
     private TilePane cardsPane;
+
+    @FXML
+    private Label lblColReport;
+
+    @FXML
+    private Label lblNumberReport;
+
+    @FXML
+    private Label lblColReportName;
 
     @FXML
     private Label lblPanel;
@@ -44,5 +60,14 @@ public class ReportsPresentation implements Initializable {
 
     private void colReport(){
 
+    }
+
+    @Override
+    public void translate(ResourceBundle resources) {
+        lblColReport.setText(resources.getString("reports.informeColecciones"));
+        lblColReportName.setText(resources.getString("reports.informeColeccionesNombre"));
+        lblNumberReport.setText(resources.getString("reports.informeNumeros"));
+        lblNumberReportCol.setText(resources.getString("reports.informeNumerosColec"));
+        lblNumberReportName.setText(resources.getString("reports.informeNumerosNombre"));
     }
 }
